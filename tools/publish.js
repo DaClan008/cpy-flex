2d357d02e496ea2fa8b7c302b029301b7b1021b3
Jan Lehmkuhl <DaClan008@users.noreply.github.com>
2020-01-30 00:58:32 +0200

    update version and publish.js

---
+++ tools/publish.js
@@ -0,0 +1,47 @@
+const { execSync } = require('child_process');
+const { writeFileSync } = require("fs");
+const { join } = require('path');
+
+const args = process.argv.slice(2);
+let version = '';
+if (args.indexOf('-p') || args.indexOf('--patch') || args.indexOf('patch')) version = 'patch';
+if (args.indexOf('-m') || args.indexOf('--minor') || args.indexOf('minor')) version = 'minor';
+if (args.indexOf('-M') || args.indexOf('--major') || args.indexOf('major')) version = 'patch';
+if (args.indexOf('--premajor') || args.indexOf('premajor')) version = 'premajor';
+if (args.indexOf('--preminor') || args.indexOf('preminor')) version = 'preminor';
+if (args.indexOf('--prepatch') || args.indexOf('prepatch')) version = 'prepatch';
+if (args.indexOf('--prerelease') || args.indexOf('prerelease')) version = 'prerelease';
+
+console.info("starting publish")
+const options = { cwd: join(process.cwd(), '/bin') }
+async function runner() {
+    let err = false;
+    if (version) {
+        try {
+            const stdout = execSync(`npm version ${version}`, options)
+            console.log(stdout);
+        } catch (error) {
+            err = true;
+            console.log(error);
+        }
+    }
+    if (!err) {
+        try {
+            const stdout = execSync('npm publish', options)
+            console.log(stdout);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    console.log("publishing completed");
+}
+
+const pack = require('../bin/package.json');
+
+if (pack && pack.version) {
+    // eslint-disable-next-line global-require
+    const currentPack = require('../package.json');
+    currentPack.version = pack.version;
+    writeFileSync("./", currentPack);
+}
+
